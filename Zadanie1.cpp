d3b0d85cb9a0dd50cd846eca1d799d0688f16311
Ni3zgodna <ola.bohonos@gmail.com>
2024-03-13 19:13:10 +0100

    Powstanie

---
+++ Zadanie1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+    float n = 1000000, s = 0;
+
+    //b
+
+    s = 1;
+
+    for(int j = 1 ; j <= n; j++)
+    {
+        s*= (4 * pow(j,2)) / ((4 * pow(j,2)) - 1);
+    }
+
+    cout << 2 * s << "\n";
+
+    s = 0;
+
+    //c
+
+    for(int j = 1; j <= n; j++)
+    {
+        s += 1 / (pow((2*j - 1), 2));
+    }
+
+    cout << sqrt(8 * s);
+
+    return 0;
+}
