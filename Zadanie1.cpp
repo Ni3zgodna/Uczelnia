092692352885c1752ead9813438d978a278bc4f0
Ni3zgodna <Ni3zgodna>
2024-03-14 11:30:10 +0100

    Lista 2 update

--- Zadanie1.cpp
+++ Zadanie1.cpp
@@ -11,16 +11,17 @@ int suma(const int tab[], unsigned size)
 
 void suma2(const int tab[], unsigned size, int* result)
 {
+    int s = * result;
     for(int i = 0; i < size; i++)
-        result += tab[i];
-    std::cout << result << " ";
+        s += tab[i];
+
+    *result = s;
 }
 
-void pisz(const int* tab, size_t size, std::ostream * out)
+void pisz(const int* tab, size_t size, std::ostream *out)
 {
-
-
-
+    for(int i = 0; i < size; i++);
+        // s += tab[i];
 }
 
 
@@ -29,20 +30,27 @@ int main()
     const int tab[9] = {1,2,3,4,5,6,7,8,9};
     unsigned size = 5;
 
+    //a
+
+    std::cout << suma(tab, size) << " ";
+
+    //b
+
     int s = 0;
     int* result;
     result = &s;
 
-    size_t size2 = 7;
-    std::ostream * out;
-
-    std::cout << suma(tab, size) << " ";
-
     suma2(tab, size, result);
-
     std::cout << s << " ";
 
-    // std::cout << pisz(tab, size, * out) << " ";
+    //c
+
+    std::ostream * out;
+    size_t size2 = 7;
+
+
+
+    pisz(tab, size, out);
 
     return 0;
 }
