cb5dd4bc25c6718169bf9ba20efbb42d62a03717
Ni3zgodna <148895287+Ni3zgodna@users.noreply.github.com>
2024-03-14 10:53:46 +0100

    Update

--- Zadanie1.cpp
+++ Zadanie1.cpp
@@ -5,6 +5,17 @@ int main()
 {
     float n = 1000000, s = 0;
 
+    //a
+    
+    s = 0;
+
+    for(int j = 1 ; j <= n; j++)
+    {
+        s+= (pow((-1), j + 1)) / (2*j - 1);
+    }
+
+    cout << 4 * s << "\n";
+    
     //b
 
     s = 1;
