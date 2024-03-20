5cb1c51947590c0362b33bd082911ad5b4e421a2
Ni3zgodna <ola.bohonos@gmail.com>
2024-03-20 18:55:04 +0100

    Nowe

--- Zadanie1.cpp
+++ Zadanie1.cpp
@@ -11,46 +11,91 @@ int suma(const int tab[], unsigned size)
 
 void suma2(const int tab[], unsigned size, int* result)
 {
-    int s = * result;
+    *result = 0;
     for(int i = 0; i < size; i++)
-        s += tab[i];
-
-    *result = s;
+        *result += tab[i];
 }
-
-void pisz(const int* tab, size_t size, std::ostream *out)
+void pisz(const int* tab, size_t size, std::ostream& out)
 {
-    for(int i = 0; i < size; i++);
-        // s += tab[i];
+    for(size_t i = 0; i < size; i++)
+        out << tab[i] << " ";
 }
 
+void iota(int tab[], unsigned size)
+{
+    for (unsigned i = 0; i < size; i++)
+        tab[i] = i;
+}
+
+void reverse(int* tab, size_t size)
+{
+    int pomocnicza;
+
+    for (size_t i = 0; i < size / 2; i++)
+    {
+        pomocnicza = tab[i];
+        tab[i] = tab[size - i - 1];
+        tab[size - i - 1] = pomocnicza;
+    }
+}
+
+int iloczyn_skalarny(const int tab1[], const int tab2[], size_t size)
+{
+    int s = 0;
+    for (size_t i = 0; i < size; i++)
+        s += tab1[i] * tab2[i];
+
+    return s;
+}
 
 int main()
 {
-    const int tab[9] = {1,2,3,4,5,6,7,8,9};
-    unsigned size = 5;
+    int tab[] = {1,2,3,4,5,6,7,8,9};
+    unsigned size = sizeof(tab) / sizeof(tab[0]);
+
 
     //a
 
-    std::cout << suma(tab, size) << " ";
+    std::cout << suma(tab, size) << "\n";
+
 
     //b
 
-    int s = 0;
-    int* result;
-    result = &s;
+    int result;
 
-    suma2(tab, size, result);
-    std::cout << s << " ";
+    suma2(tab, size, &result);
+    std::cout << result << "\n";
+
 
     //c
 
-    std::ostream * out;
-    size_t size2 = 7;
+    pisz(tab, size, std::cout);
+    std::cout << "\n";
 
 
+    //d
 
-    pisz(tab, size, out);
+    iota(tab, size);
+    for (unsigned i = 0; i < size; i++)
+        std::cout << tab[i] << " ";
+    std::cout << "\n";
+
+
+    //e
+
+    reverse(tab, size);
+    for (unsigned i = 0; i < size; i++)
+        std::cout << tab[i] << " ";
+    std::cout << "\n";
+
+
+    //f
+
+    int tab1[] = {1, 2};
+    int tab2[] = {3, 4};
+    unsigned size1 = sizeof(tab1) / sizeof(tab1[0]);
+
+    std::cout << iloczyn_skalarny(tab1, tab2, size1);
 
     return 0;
 }
