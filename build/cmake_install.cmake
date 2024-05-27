d5d089bc390a22780b9a8a7ff5cf5e53801c477e
Ni3zgodna <ola.bohonos@gmail.com>
2024-05-27 20:40:59 +0200

    Lista 5

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/Uczelnia/Uczelnia/Uczelnia/build/CMakeFiles/CMakeScratch/TryCompile-3mxl9a
+# Install script for directory: C:/Uczelnia/Uczelnia/Uczelnia/Lista5/Lista5
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/CMAKE_TRY_COMPILE")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/image")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Uczelnia/Uczelnia/Uczelnia/build/CMakeFiles/CMakeScratch/TryCompile-3mxl9a/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Uczelnia/Uczelnia/Uczelnia/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
